40df1bdd359b1f9f6105b186f274de666bcdae8f
boxfish <boxfish893@gmail.com>
2024-11-04 00:59:34 +0900

    feat: 来場者メッセージなども取得できるように変更 / #1

--- talkMate/src/comment/getNiconico.ts
+++ talkMate/src/comment/getNiconico.ts
@@ -1,49 +1,55 @@
-import cheerio from "cheerio";
 import { NicoliveClient } from "@kikurage/nicolive-api/node";
+import * as cheerio from "cheerio";
 import { createChat } from "../message/opeMess";
 
 export const getNiconico = async (userId: string) => {
-  const pageText = await fetcher(
-    "https://live.nicovideo.jp/watch/user/" + userId
-  );
-  const nowliveId = await getLiveId(pageText);
+	const pageText = await fetcher(
+		`https://live.nicovideo.jp/watch/user/${userId}`,
+	);
+	const nowliveId = await getLiveId(pageText);
 
-  new NicoliveClient({ liveId: nowliveId })
-    .on("chat", (chat) => {
-      console.log("niconico:", chat.content);
-      createChat("viewer", chat.content);
-    })
-    .on("simpleNotification", (notification) => {})
-    .on("changeState", (state) => {
-      const nusiCome = state.marque?.display?.operatorComment?.content;
-      if (nusiCome) {
-        console.log("niconico:", nusiCome);
-        createChat("fuguo", nusiCome);
-      }
-    })
-    .connect();
+	new NicoliveClient({ liveId: nowliveId })
+		.on("chat", (chat) => {
+			console.log("niconico:", chat.content);
+			createChat("viewer", chat.content);
+		})
+		.on("simpleNotification", (notification) => {
+			const message = notification.message.value;
+			if (message) {
+				console.log("niconico:", message);
+				createChat("viewer", message);
+			}
+		})
+		.on("changeState", (state) => {
+			const nusiCome = state.marque?.display?.operatorComment?.content;
+			if (nusiCome) {
+				console.log("niconico:", nusiCome);
+				createChat("fuguo", nusiCome);
+			}
+		})
+		.connect();
 };
 
 const fetcher = async (url: string) => {
-  for (let i = 0; i < 5; i++) {
-    try {
-      const response = await fetch(url);
-      const page = await response.text();
-      return page;
-    } catch (e) {
-      console.log(e);
-    }
-    await new Promise((resolve) => setTimeout(resolve, 100));
-  }
-  throw new Error("Failed to fetch");
+	for (let i = 0; i < 5; i++) {
+		try {
+			const response = await fetch(url);
+			const page = await response.text();
+			return page;
+		} catch (e) {
+			console.log(e);
+		}
+		await new Promise((resolve) => setTimeout(resolve, 100));
+	}
+	throw new Error("Failed to fetch");
 };
 
 export const getLiveId = async (pageText: string) => {
-  const $ = cheerio.load(pageText);
-  const liveUrl = $('meta[property="og:url"]').attr("content");
-  if (!liveUrl) {
-    throw new Error("liveId not found");
-  }
-  const liveId = liveUrl.replace("https://live.nicovideo.jp/watch/", "");
-  return liveId;
+	const $ = cheerio.load(pageText);
+	const liveUrl = $('meta[property="og:url"]').attr("content");
+	if (!liveUrl) {
+		throw new Error("liveId not found");
+	}
+	const liveId = liveUrl.replace("https://live.nicovideo.jp/watch/", "");
+	return liveId;
 };
