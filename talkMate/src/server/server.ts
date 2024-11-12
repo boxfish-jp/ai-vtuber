24fa0b5a532cadc0d49c09962c1a493dcf4cd072
boxfish <boxfish893@gmail.com>
2024-11-12 23:38:16 +0900

    fix: スクショが常時取られるバグを修正

--- talkMate/src/server/server.ts
+++ talkMate/src/server/server.ts
@@ -7,39 +7,39 @@ import { sendAPI } from "./sendAPI";
 import { initSocketServer } from "./socketServer";
 
 export const startServer = () => {
-	const app = new Hono();
+  const app = new Hono();
 
-	app.get("/", async (c) => {
-		const chatId = Number(c.req.query("id"));
-		const needScreenshot = Boolean(c.req.query("screenshot"));
-		if (!chatId) {
-			return c.text("chatId is required", 400);
-		}
-		const response = await sendAPI(chatId, needScreenshot);
-		await createChat("ai", response);
-		return c.text(response);
-	});
-	app.use(
-		"*",
-		serveStatic({
-			root: "public",
-			onNotFound: (path, c) => {
-				console.log(`${path} not found`);
-			},
-		}),
-	);
+  app.get("/", async (c) => {
+    const chatId = Number(c.req.query("id"));
+    const needScreenshot = c.req.query("screenshot") === "true";
+    if (!chatId) {
+      return c.text("chatId is required", 400);
+    }
+    const response = await sendAPI(chatId, needScreenshot);
+    await createChat("ai", response);
+    return c.text(response);
+  });
+  app.use(
+    "*",
+    serveStatic({
+      root: "public",
+      onNotFound: (path, c) => {
+        console.log(`${path} not found`);
+      },
+    }),
+  );
 
-	console.log(
-		`webPage is running on  http://${talkMateEndpoint.ip}:${Number(
-			talkMateEndpoint.port,
-		)}/index.html`,
-	);
+  console.log(
+    `webPage is running on  http://${talkMateEndpoint.ip}:${Number(
+      talkMateEndpoint.port,
+    )}/index.html`,
+  );
 
-	const server = serve({
-		fetch: app.fetch,
-		hostname: talkMateEndpoint.ip,
-		port: Number(talkMateEndpoint.port),
-	});
+  const server = serve({
+    fetch: app.fetch,
+    hostname: talkMateEndpoint.ip,
+    port: Number(talkMateEndpoint.port),
+  });
 
-	initSocketServer(server);
+  initSocketServer(server);
 };
