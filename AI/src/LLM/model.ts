c5861c51c4dae5adc2fb06dd79daf44a6c6d3ffe
boxfish_jp <boxfish893@gmail.com>
2024-12-19 23:14:42 +0900

    feat: geminiにロケーション設定とsafety設定を追加

--- AI/src/LLM/model.ts
+++ AI/src/LLM/model.ts
@@ -2,28 +2,45 @@ import { ChatOllama } from "@langchain/community/chat_models/ollama";
 import { ChatVertexAI } from "@langchain/google-vertexai-web";
 
 export const getlocalModel = async () => {
-	if (!_localModel) {
-		_localModel = new ChatOllama({
-			model: "gemma_9b",
-		});
-	}
-	return _localModel;
+  if (!_localModel) {
+    _localModel = new ChatOllama({
+      model: "gemma_9b",
+    });
+  }
+  return _localModel;
 };
 
 let _localModel: ChatOllama;
 
-const getGemini = () => {
-	const gemini = new ChatVertexAI({
-		model: "gemini-1.5-flash",
-		maxOutputTokens: 50,
-		safetySettings: [
-			{
-				category: "HARM_CATEGORY_UNSPECIFIED",
-				threshold: "HARM_BLOCK_THRESHOLD_UNSPECIFIED",
-			},
-		],
-	});
-	return gemini;
+const getGemini = (modelName: string) => {
+  const gemini = new ChatVertexAI({
+    model: modelName,
+    maxOutputTokens: 50,
+    location: "asia-northeast1",
+    safetySettings: [
+      {
+        category: "HARM_CATEGORY_HARASSMENT",
+        threshold: "BLOCK_ONLY_HIGH",
+      },
+      {
+        category: "HARM_CATEGORY_HATE_SPEECH",
+        threshold: "BLOCK_ONLY_HIGH",
+      },
+      {
+        category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        threshold: "BLOCK_ONLY_HIGH",
+      },
+      {
+        category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+        threshold: "BLOCK_ONLY_HIGH",
+      },
+      {
+        category: "HARM_CATEGORY_CIVIC_INTEGRITY",
+        threshold: "BLOCK_ONLY_HIGH",
+      },
+    ],
+  });
+  return gemini;
 };
 
-export const gemini = getGemini();
+export const gemini = getGemini("gemini-1.5-flash");
