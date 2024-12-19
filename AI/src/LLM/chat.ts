2599bddda7e9de5534fa443d2956ec55a48486a2
boxfish_jp <boxfish893@gmail.com>
2024-12-19 23:16:18 +0900

    feat: geminiの応答速度が改善されたので、ローカルモデルをchat機能で使うのをやめた

--- AI/src/LLM/chat.ts
+++ AI/src/LLM/chat.ts
@@ -1,6 +1,6 @@
 import { StringOutputParser } from "@langchain/core/output_parsers";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { getlocalModel, gemini } from "./model";
+import { gemini } from "./model";
 import AIConfig from "../../AIConfig.json";
 import { sleep } from "../lib/sleep";
 import type { chatHistoryType } from "../type/chatHistoryType";
@@ -10,36 +10,33 @@ import { createExamplePrompt } from "./util/createExamplePrompt";
 import { readFileContent } from "../../prompt/readFileContent";
 
 export const chat = async (
-	chatHistory: chatHistoryType,
-	imageUrl: string | undefined,
+  chatHistory: chatHistoryType,
+  imageUrl: string | undefined
 ): Promise<string> => {
-	const messages = createMessages(chatHistory);
-	const inputPrompt = createInputPrompt(chatHistory, imageUrl);
-	const systemPrompt = await readFileContent("prompt/chat/system.md");
-	const examplePrompt = await createExamplePrompt("prompt");
+  const messages = createMessages(chatHistory);
+  const inputPrompt = createInputPrompt(chatHistory, imageUrl);
+  const systemPrompt = await readFileContent("prompt/chat/system.md");
+  const examplePrompt = await createExamplePrompt("prompt");
 
-	const prompt = ChatPromptTemplate.fromMessages([
-		["system", systemPrompt],
-		["placeholder", "{messages}"],
-		examplePrompt,
-		inputPrompt,
-	]);
+  const prompt = ChatPromptTemplate.fromMessages([
+    ["system", systemPrompt],
+    ["placeholder", "{messages}"],
+    examplePrompt,
+    inputPrompt,
+  ]);
 
-	const parser = new StringOutputParser();
-	const localModel = await getlocalModel();
-	const chain = imageUrl
-		? prompt.pipe(gemini).pipe(parser)
-		: prompt.pipe(localModel).pipe(parser);
-	for (let i = 0; i < AIConfig.prompt.model.maxRetries; i++) {
-		try {
-			const response = await chain.invoke({
-				messages: messages,
-			});
-			return response.replace(/[\r\n]+/g, "");
-		} catch (e) {
-			console.log("error:", e);
-			await sleep(1000);
-		}
-	}
-	return "思考が停止しました";
+  const parser = new StringOutputParser();
+  const chain = prompt.pipe(gemini).pipe(parser);
+  for (let i = 0; i < AIConfig.prompt.model.maxRetries; i++) {
+    try {
+      const response = await chain.invoke({
+        messages: messages,
+      });
+      return response.replace(/[\r\n]+/g, "");
+    } catch (e) {
+      console.log("error:", e);
+      await sleep(1000);
+    }
+  }
+  return "思考が停止しました";
 };
