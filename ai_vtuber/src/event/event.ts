d3f3bfb69a45f9b7ff435ce456ad9795339d057a
boxfish_jp <boxfish893@gmail.com>
2025-01-18 16:48:57 +0900

    refactor: serverをeventに変えた。eventクラスを追加。

---
+++ ai_vtuber/src/event/event.ts
@@ -0,0 +1,27 @@
+interface chatEvent {
+	who: "ai" | "fuguo" | "viewer" | "info";
+	content: string;
+	unixTime: number;
+	point: boolean;
+}
+
+interface instructionEvent {
+	type: "talk" | "work_theme" | "afk" | "back" | "grade";
+	unixTime: number;
+	needScreenshot: boolean;
+}
+
+export class LiveEvent {
+	readonly chat: chatEvent | undefined = undefined;
+	readonly instruction: instructionEvent | undefined = undefined;
+	readonly fuguoSpeaking: boolean | undefined = undefined;
+	constructor(
+		chat: chatEvent | undefined,
+		instruction: instructionEvent | undefined,
+		fuguoSpeaking: boolean | undefined,
+	) {
+		this.chat = chat;
+		this.instruction = instruction;
+		this.fuguoSpeaking = fuguoSpeaking;
+	}
+}
---
+++ ai_vtuber/src/event/server.ts
@@ -0,0 +1,82 @@
+import type { Server as HttpServer } from "node:http";
+import { serve } from "@hono/node-server";
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { Server } from "socket.io";
+import { z } from "zod";
+import endpointJson from "../../../endpoint.json";
+import { LiveEvent } from "./event.js";
+
+const chatEventSchema = z.object({
+	who: z.enum(["ai", "fuguo", "viewer", "info"]),
+	content: z.string(),
+	unixTime: z.number(),
+	point: z.boolean(),
+});
+
+const instructionEventSchema = z.object({
+	type: z.enum(["talk", "work_theme", "afk", "back", "grade"]),
+	unixTime: z.number(),
+	needScreenshot: z.boolean(),
+});
+
+let eventListener = (event: LiveEvent) => {
+	console.log(event);
+};
+
+const app = new Hono();
+const appChatPost = app.post(
+	"/chat",
+	zValidator("json", chatEventSchema),
+	(c) => {
+		const data = c.req.valid("json");
+		const newEvent = new LiveEvent(data, undefined, undefined);
+		eventListener(newEvent);
+		return c.text("ok");
+	},
+);
+
+export type appChatPostType = typeof appChatPost;
+
+const server = serve(
+	{
+		fetch: app.fetch,
+		hostname: endpointJson.ai_vtuber.address,
+		port: Number(endpointJson.ai_vtuber.port),
+	},
+	(info) => {
+		console.log(`server is running on  http://${info.address}:${info.port}`);
+	},
+);
+
+const ioServer = new Server(server as HttpServer, {
+	path: "/ws",
+	serveClient: false,
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+});
+
+export const eventServer = (onNewEvent: (event: LiveEvent) => void) => {
+	eventListener = onNewEvent;
+	ioServer.on("connection", (socket) => {
+		socket.on("chat", (msg: string) => {
+			const receivedMessage = chatEventSchema.parse(JSON.parse(msg));
+			const newEvent = new LiveEvent(receivedMessage, undefined, undefined);
+			eventListener(newEvent);
+		});
+
+		socket.on("speak", (msg: string): void => {
+			const newEvent = new LiveEvent(undefined, undefined, msg === "true");
+			eventListener(newEvent);
+		});
+
+		socket.on("start", (msg: string): void => {
+			const instruction = instructionEventSchema.parse(JSON.parse(msg));
+			console.log("receivedMessage");
+			const newEvent = new LiveEvent(undefined, instruction, undefined);
+			eventListener(newEvent);
+		});
+	});
+};
