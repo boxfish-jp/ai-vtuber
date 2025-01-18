90edf0209b6774ac8c18a14c36bdee7b15715f5e
boxfish_jp <boxfish893@gmail.com>
2025-01-18 20:43:09 +0900

    feat: aiStateのtalkingのsetの仕様を変更

--- ai_vtuber/src/state/ai.ts
+++ ai_vtuber/src/state/ai.ts
@@ -7,8 +7,8 @@ export class AiState implements talkStateDataType {
 		return this._talking === 0;
 	}
 
-	set talking(bool: boolean) {
-		this._talking = bool ? 0 : new Date().getTime();
+	setTalking() {
+		this._talking = new Date().getTime();
 	}
 	get silence() {
 		return new Date().getTime() - this._talking > 15000 && !this.talking;
