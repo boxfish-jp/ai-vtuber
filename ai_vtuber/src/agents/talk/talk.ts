31c0457301bb81e732256dcf6e059a37b3f9f3d0
boxfish_jp <boxfish893@gmail.com>
2025-01-19 16:51:42 +0900

    refactor: talkをAgent化

--- ai_vtuber/src/agents/talk/talk.ts
+++ ai_vtuber/src/agents/talk/talk.ts
@@ -2,7 +2,7 @@ import { StringOutputParser } from "@langchain/core/output_parsers";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import type { Activity } from "../../activity/activity.js";
 import { gemini } from "../../lib/model.js";
-import { getAiState } from "../../state/ai.js";
+import type { Agent } from "../agent.js";
 import { cleanLlmResponse } from "../llm_response_cleaner.js";
 import { convertToExamplePrompt } from "./lib/convert/example.js";
 import {
@@ -10,36 +10,34 @@ import {
 	getTalkSystemPrompt,
 } from "./prompt/talk.js";
 
-export const talk = async (activity: Activity): Promise<string | undefined> => {
-	const aiState = getAiState();
-	if (aiState.talking) {
-		return undefined;
-	}
-	const chatHistoryPrompt = activity.chatHistoryPrompt;
-	const inputPrompt = activity.inputPrompt;
-	const systemPrompt = getTalkSystemPrompt(chatHistoryPrompt);
-	const examplePrompt = await convertToExamplePrompt(
-		getTalkExamplePromptData(),
-	);
-	const prompt = ChatPromptTemplate.fromMessages([
-		["system", "{system}"],
-		examplePrompt,
-		inputPrompt,
-	]);
+export class Talk implements Agent {
+	async service(activity: Activity): Promise<string> {
+		const chatHistoryPrompt = activity.chatHistoryPrompt;
+		const inputPrompt = activity.inputPrompt;
+		const systemPrompt = getTalkSystemPrompt(chatHistoryPrompt);
+		const examplePrompt = await convertToExamplePrompt(
+			getTalkExamplePromptData(),
+		);
+		const prompt = ChatPromptTemplate.fromMessages([
+			["system", "{system}"],
+			examplePrompt,
+			inputPrompt,
+		]);
 
-	const parser = new StringOutputParser();
-	const chain = prompt.pipe(gemini).pipe(parser);
-	try {
-		const response = await chain.invoke({
-			system: systemPrompt,
-		});
-		const cleanedResponse = cleanLlmResponse(response);
-		if (cleanedResponse.length > 10) {
-			return undefined;
+		const parser = new StringOutputParser();
+		const chain = prompt.pipe(gemini).pipe(parser);
+		try {
+			const response = await chain.invoke({
+				system: systemPrompt,
+			});
+			const cleanedResponse = cleanLlmResponse(response);
+			if (cleanedResponse.length > 10) {
+				return "";
+			}
+			return cleanedResponse;
+		} catch (e) {
+			console.log("error:", e);
+			return "思考が停止しました。";
 		}
-		return cleanedResponse;
-	} catch (e) {
-		console.log("error:", e);
-		return "思考が停止しました。";
 	}
-};
+}
