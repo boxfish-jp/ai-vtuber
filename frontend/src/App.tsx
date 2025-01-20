9cf486049f59fe238f5441e990f12c2b579d1afd
boxfish_jp <boxfish893@gmail.com>
2025-01-20 21:39:08 +0900

    style: frontendのformatかかってなくてどえらいことになった。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Label } from "@radix-ui/react-label";
 import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -8,167 +9,168 @@ import { RadioGroup, RadioGroupItem } from "./components/ui/radio-group";
 import { ScrollArea } from "./components/ui/scroll-area";
 import { Switch } from "./components/ui/switch";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "./components/ui/table";
 import { Toaster } from "./components/ui/toaster";
 import { useToast } from "./hooks/use-toast";
 import { sendEvent, type socketServerChatType, watchChat } from "./lib/socket";
 import { speechRecognition } from "./lib/speechrecognition";
-import { Label } from "@radix-ui/react-label";
 
 const formSchema = z.object({
-	type: z.enum(["talk", "work_theme", "afk", "back"]).default("talk"),
-	unixTime: z.string(),
-	needScreenshot: z.boolean().default(false),
+  type: z.enum(["talk", "work_theme", "afk", "back", "grade"]).default("talk"),
+  unixTime: z.string(),
+  needScreenshot: z.boolean().default(false),
 });
 
 function App() {
-	const [chats, setChats] = useState<socketServerChatType[]>([]);
-	const listEndRef = useRef<HTMLTableRowElement>(null);
-	const { toast } = useToast();
+  const [chats, setChats] = useState<socketServerChatType[]>([]);
+  const listEndRef = useRef<HTMLTableRowElement>(null);
+  const { toast } = useToast();
 
-	useEffect(() => {
-		speechRecognition((eventName, content) => {
-			sendEvent(eventName, content);
-		});
-		watchChat((chat) => {
-			setChats((prev) => [...prev, chat]);
-			listEndRef.current?.scrollIntoView({ behavior: "smooth" });
-			console.log("chats", chats);
-		});
-	}, []);
+  useEffect(() => {
+    speechRecognition((eventName, content) => {
+      sendEvent(eventName, content);
+    });
+    watchChat((chat) => {
+      setChats((prev) => [...prev, chat]);
+      listEndRef.current?.scrollIntoView({ behavior: "smooth" });
+      console.log("chats", chats);
+    });
+  }, []);
 
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-	});
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
 
-	const onSubmit = async (data: z.infer<typeof formSchema>) => {
-		console.log("submit");
-		toast({
-			title: "AIに送信しました",
-		});
-		sendEvent(
-			"instruction",
-			JSON.stringify({
-				type: data.type,
-				unixTime: data.unixTime,
-				needScreenshot: data.needScreenshot,
-			}),
-		);
-		form.setValue("needScreenshot", false);
-		form.setValue("type", "talk");
-	};
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    console.log("submit");
+    toast({
+      title: "AIに送信しました",
+    });
+    sendEvent(
+      "instruction",
+      JSON.stringify({
+        type: data.type,
+        unixTime: data.unixTime,
+        needScreenshot: data.needScreenshot,
+      }),
+    );
+    form.setValue("needScreenshot", false);
+    form.setValue("type", "talk");
+  };
 
-	return (
-		<div>
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className="flex flex-col gap-6 items-center"
-				>
-					<div className="self-start mx-0 w-full">
-						<FormField
-							control={form.control}
-							name="unixTime"
-							render={({ field }) => (
-								<FormItem>
-									<FormControl>
-										<RadioGroup
-											onValueChange={(value) => {
-												field.onChange(value);
-												form.handleSubmit(onSubmit)();
-											}}
-											defaultValue={field.value?.toString()}
-										>
-											<ScrollArea className=" h-[80vh]">
-												<Table>
-													<TableHeader>
-														<TableRow>
-															<TableHead className="w-10">選択</TableHead>
-															<TableHead className="w-16">id</TableHead>
-															<TableHead className="w-16">who</TableHead>
-															<TableHead>内容</TableHead>
-														</TableRow>
-													</TableHeader>
+  return (
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-6 items-center"
+        >
+          <div className="self-start mx-0 w-full">
+            <FormField
+              control={form.control}
+              name="unixTime"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={(value) => {
+                        field.onChange(value);
+                        form.handleSubmit(onSubmit)();
+                      }}
+                      defaultValue={field.value?.toString()}
+                    >
+                      <ScrollArea className=" h-[80vh]">
+                        <Table>
+                          <TableHeader>
+                            <TableRow>
+                              <TableHead className="w-10">選択</TableHead>
+                              <TableHead className="w-16">id</TableHead>
+                              <TableHead className="w-16">who</TableHead>
+                              <TableHead>内容</TableHead>
+                            </TableRow>
+                          </TableHeader>
 
-													<TableBody>
-														{chats.map((chat) => (
-															<TableRow key={chat.unixTime.toString()}>
-																<TableCell>
-																	<RadioGroupItem
-																		value={chat.unixTime.toString()}
-																	/>
-																</TableCell>
-																<TableCell>{chat.unixTime}</TableCell>
-																<TableCell>{chat.who}</TableCell>
-																<TableCell>{chat.content}</TableCell>
-															</TableRow>
-														))}
-														<TableRow ref={listEndRef} />
-													</TableBody>
-												</Table>
-											</ScrollArea>
-										</RadioGroup>
-									</FormControl>
-								</FormItem>
-							)}
-						/>
-					</div>
-					<div className="flex items-center gap-2">
-						<FormField
-							control={form.control}
-							name="needScreenshot"
-							render={({ field }) => (
-								<FormItem>
-									<FormControl>
-										<Switch
-											checked={field.value}
-											onCheckedChange={field.onChange}
-										/>
-									</FormControl>
-								</FormItem>
-							)}
-						/>
-						<p>スクショ</p>
-					</div>
-					<FormField
-						control={form.control}
-						name="type"
-						render={({ field }) => (
-							<FormItem>
-								<FormControl>
-									<RadioGroup
-										value={field.value}
-										onValueChange={field.onChange}
-										defaultValue="talk"
-										className="flex gap-4"
-									>
-										{["talk", "work_theme", "afk", "back"].map((type) => (
-											<div key={type}>
-												<RadioGroupItem value={type} id={type} />
-												<Label className="ms-1" htmlFor={type}>
-													{type}
-												</Label>
-											</div>
-										))}
-									</RadioGroup>
-								</FormControl>
-							</FormItem>
-						)}
-					/>
-					<Button type="submit" className="w-48" size={"lg"}>
-						送信
-					</Button>
-				</form>
-			</Form>
-			<Toaster />
-		</div>
-	);
+                          <TableBody>
+                            {chats.map((chat) => (
+                              <TableRow key={chat.unixTime.toString()}>
+                                <TableCell>
+                                  <RadioGroupItem
+                                    value={chat.unixTime.toString()}
+                                  />
+                                </TableCell>
+                                <TableCell>{chat.unixTime}</TableCell>
+                                <TableCell>{chat.who}</TableCell>
+                                <TableCell>{chat.content}</TableCell>
+                              </TableRow>
+                            ))}
+                            <TableRow ref={listEndRef} />
+                          </TableBody>
+                        </Table>
+                      </ScrollArea>
+                    </RadioGroup>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <FormField
+              control={form.control}
+              name="needScreenshot"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <p>スクショ</p>
+          </div>
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <RadioGroup
+                    value={field.value}
+                    onValueChange={field.onChange}
+                    defaultValue="talk"
+                    className="flex gap-4"
+                  >
+                    {["talk", "work_theme", "afk", "back", "grade"].map(
+                      (type) => (
+                        <div key={type}>
+                          <RadioGroupItem value={type} id={type} />
+                          <Label className="ms-1" htmlFor={type}>
+                            {type}
+                          </Label>
+                        </div>
+                      ),
+                    )}
+                  </RadioGroup>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-48" size={"lg"}>
+            送信
+          </Button>
+        </form>
+      </Form>
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
