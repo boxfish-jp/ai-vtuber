f183c9547556fb32b4911c02e0cf3c3266955cb8
boxfish_jp <boxfish893@gmail.com>
2025-01-21 22:55:57 +0900

    feat: work_themeエージェントのプログラムをシンプルに

--- ai_vtuber/src/agents/work_theme/work_theme.ts
+++ ai_vtuber/src/agents/work_theme/work_theme.ts
@@ -9,16 +9,12 @@ import { workThemeSystemPrompt } from "./prompt.js";
 
 export class WorkTheme implements Agent {
 	private _theme = "";
-	private checking: undefined | string = undefined;
 
 	get theme(): string {
 		return this._theme;
 	}
 
 	async service(activity: Activity): Promise<AgentResponse> {
-		if (this.checking) {
-			return await this.check(activity);
-		}
 		return await this.consult(activity);
 	}
 
@@ -32,14 +28,16 @@ export class WorkTheme implements Agent {
 			inputPrompt,
 		]);
 
-		const modelWithTools = gemini.bindTools([this.affect]);
+		const modelWithTools = gemini.bindTools([this.confirm]);
 		const chain = prompt.pipe(modelWithTools);
 		try {
 			const response = await chain.invoke({
 				system: systemPrompt,
 			});
 			if (response.tool_calls?.length) {
-				const toolMessage = await this.affect.invoke(response.tool_calls[0]);
+				const toolName = response.tool_calls[0].name as keyof typeof this.tools;
+				const tool = this.tools[toolName];
+				const toolMessage = await tool.invoke(response.tool_calls[0]);
 				return {
 					text:
 						typeof response.content === "string"
@@ -53,7 +51,7 @@ export class WorkTheme implements Agent {
 					typeof response.content === "string"
 						? cleanLlmResponse(response.content)
 						: "",
-				completed: true,
+				completed: false,
 			};
 		} catch (e) {
 			console.log("error:", e);
@@ -64,71 +62,24 @@ export class WorkTheme implements Agent {
 		}
 	}
 
-	private async check(activity: Activity): Promise<AgentResponse> {
-		const chatHistoryPrompt = activity.chatHistoryPrompt;
-		const inputPrompt = activity.inputPrompt;
-		const systemPrompt = workThemeSystemPrompt(chatHistoryPrompt);
-
-		const prompt = ChatPromptTemplate.fromMessages([
-			["system", "{system}"],
-			inputPrompt,
-		]);
-
-		const modelWithTools = gemini.bindTools([this.confirm, this.affect]);
-		const chain = prompt.pipe(modelWithTools);
-		try {
-			const response = await chain.invoke({
-				system: systemPrompt,
-			});
-			if (response.tool_calls?.length) {
-				const toolMessage = await this.affect.invoke(response.tool_calls[0]);
-				return { text: toolMessage.content, completed: true };
-			}
-			return {
-				text:
-					typeof response.content === "string"
-						? cleanLlmResponse(response.content)
-						: "",
-				completed: true,
-			};
-		} catch (e) {
-			console.log("error:", e);
-			return { text: "思考が停止しました。", completed: false };
-		}
-	}
-
-	private affect = tool(
-		({ input }: { input: string }): string => {
-			this.checking = input;
-			return `つまり、現在の内容は「${this.checking}」であってる？`;
-		},
-		{
-			name: "affect",
-			description:
-				"inputに現在の作業内容の概要を入れると、これ以後の会話のシステムプロンプトに今日の作業内容が反映されるようになります。また、視聴者も現在の作業内容の把握が容易になります。",
-			schema: z.object({
-				input: z.string(),
-			}),
-		},
-	);
-
 	private confirm = tool(
-		(): string => {
-			if (this.checking !== undefined) {
-				this._theme = this.checking;
-				this.checking = undefined;
-			}
+		({ input }: { input: string }): string => {
+			this._theme = input;
 			return `作業内容を「${this._theme}」に更新したよ。`;
 		},
 		{
 			name: "confirm",
 			description:
-				"ユーザーがあなたの提案した作業内容の要約に賛成している場合は、inputにyesと入れると、その内容が確定されます。",
+				"inputに作業内容の要約を入力すると、その作業内容が確定されます。",
 			schema: z.object({
 				input: z.string(),
 			}),
 		},
 	);
+
+	private tools = {
+		confirm: this.confirm,
+	};
 }
 
 let workTheme: WorkTheme | undefined = undefined;
