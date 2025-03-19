b9476d6dac4c1dbae32909964717ceb85e2a3936
boxfish_jp <boxfish893@gmail.com>
2025-03-19 21:58:31 +0900

    refactor: frontendのuiとロジックを分けた

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,17 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Label } from "@radix-ui/react-label";
 import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { ChatTable } from "./Chat_table";
+import { Controller } from "./Controller";
 import { Button } from "./components/ui/button";
-import { Form, FormControl, FormField, FormItem } from "./components/ui/form";
-import { RadioGroup, RadioGroupItem } from "./components/ui/radio-group";
-import { ScrollArea } from "./components/ui/scroll-area";
-import { Switch } from "./components/ui/switch";
-import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from "./components/ui/table";
+import { Form } from "./components/ui/form";
 import { Toaster } from "./components/ui/toaster";
 import { useToast } from "./hooks/use-toast";
 import { SocketControler, type socketServerChatType } from "./lib/socket";
 import { Recognition } from "./lib/speechrecognition";
 
-const formSchema = z.object({
+export const formSchema = z.object({
 	type: z.enum(["talk", "work_theme", "afk", "back", "grade"]).default("talk"),
 	unixTime: z.string().default(""),
 	needScreenshot: z.boolean().default(false),
@@ -79,104 +69,13 @@ function App() {
 					onSubmit={form.handleSubmit(onSubmit)}
 					className="flex flex-col gap-6 items-center"
 				>
-					<div className="self-start mx-0 w-full">
-						<FormField
-							control={form.control}
-							name="unixTime"
-							render={({ field }) => (
-								<FormItem>
-									<FormControl>
-										<RadioGroup
-											onValueChange={(value) => {
-												field.onChange(value);
-												form.handleSubmit(onSubmit)();
-											}}
-											defaultValue={field.value?.toString()}
-										>
-											<ScrollArea className=" h-[80vh]">
-												<Table>
-													<TableHeader>
-														<TableRow>
-															<TableHead className="w-10">選択</TableHead>
-															<TableHead className="w-16">id</TableHead>
-															<TableHead className="w-16">who</TableHead>
-															<TableHead>内容</TableHead>
-														</TableRow>
-													</TableHeader>
-
-													<TableBody>
-														{chats.map((chat) => (
-															<TableRow key={chat.unixTime.toString()}>
-																<TableCell>
-																	<RadioGroupItem
-																		value={chat.unixTime.toString()}
-																	/>
-																</TableCell>
-																<TableCell>{chat.unixTime}</TableCell>
-																<TableCell>{chat.who}</TableCell>
-																<TableCell>{chat.content}</TableCell>
-															</TableRow>
-														))}
-														<TableRow ref={listEndRef} />
-													</TableBody>
-												</Table>
-											</ScrollArea>
-										</RadioGroup>
-									</FormControl>
-								</FormItem>
-							)}
-						/>
-					</div>
-					<div className="flex items-center gap-2">
-						<FormField
-							control={form.control}
-							name="needScreenshot"
-							render={({ field }) => (
-								<FormItem>
-									<FormControl>
-										<Switch
-											checked={field.value}
-											onCheckedChange={field.onChange}
-										/>
-									</FormControl>
-								</FormItem>
-							)}
-						/>
-						<p>スクショ</p>
-					</div>
-					<FormField
-						control={form.control}
-						name="type"
-						render={({ field }) => (
-							<FormItem>
-								<FormControl>
-									<RadioGroup
-										value={field.value}
-										onValueChange={field.onChange}
-										defaultValue="talk"
-										className="flex gap-4"
-									>
-										{[
-											"talk",
-											"work_theme",
-											"afk",
-											"back",
-											"grade",
-											"reminder",
-											"reset",
-										].map((type) => (
-											<div key={type}>
-												<RadioGroupItem value={type} id={type} />
-												<Label className="ms-1" htmlFor={type}>
-													{type}
-												</Label>
-											</div>
-										))}
-									</RadioGroup>
-								</FormControl>
-							</FormItem>
-						)}
+					<ChatTable
+						chats={chats}
+						form={form}
+						onSubmit={onSubmit}
+						listEndRef={listEndRef}
 					/>
+					<Controller form={form} />
 					<Button type="submit" className="w-48" size={"lg"}>
 						送信
 					</Button>
