35680f66aec12209fa20fdac26b6c3cd77e993ff
boxfish_jp <boxfish893@gmail.com>
2025-04-05 01:14:27 +0900

    feat: 過去のプロダクト一覧をbiim画面から見れるように

--- obs/src/biim.tsx
+++ obs/src/biim.tsx
@@ -1,14 +1,54 @@
+import { useEffect, useState } from "react";
+import supplementJson from "../../supplement.json";
 import type { WorkTheme } from "./lib/socket";
 
 export const Biim = ({
 	subtitle,
 	workTheme,
 }: { subtitle: string; workTheme: WorkTheme }) => {
+	const [supplement, setSupplement] = useState({
+		content: "",
+		index: 0,
+	});
+	const [interval, setNewInterval] = useState<NodeJS.Timeout | undefined>(
+		undefined,
+	);
+
+	useEffect(() => {
+		if (interval) {
+			clearInterval(interval);
+		}
+		setNewInterval(
+			setInterval(() => {
+				const supplements = supplementParser(workTheme.main);
+				const index =
+					subtitle || supplement.index >= supplements.length - 1
+						? 0
+						: supplement.index + 1;
+				setSupplement({
+					content: supplements[index],
+					index: index,
+				});
+			}, 20000),
+		);
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [supplement, workTheme, subtitle]);
+
 	return (
 		<main className="w-[854px] h-[480px] bg-black flex flex-row items-end gap-1 p-1">
-			<div className="w-full h-[70px] flex flex-row gap-1 justify-end">
-				<div className="w-[550px] border-[3px] border-white text-white text-center">
-					{subtitle}
+			<div className="w-full h-[80px] flex flex-row gap-1 justify-end">
+				<div
+					className={`w-[550px] border-[3px] border-white text-white p-3 ${subtitle ? "text-center" : "text-xs"}`}
+				>
+					{subtitle
+						? subtitle
+						: supplement.content
+								.split("\n")
+								.filter((content) => content)
+								.map((content) => <p key={content}>{content}</p>)}
 				</div>
 			</div>
 			<div className="w-[155px] h-full flex flex-col gap-1">
@@ -38,4 +78,55 @@ export const Biim = ({
 			</div>
 		</main>
 	);
+};
+
+const supplementParser = (word: string) => {
+	const past: string[] = [];
+	const now: string[] = [];
+	supplementJson.制作物.map((work) => {
+		work.description.map((content) => {
+			if (work.category.some((keyword) => word.includes(keyword))) {
+				now.push(`
+現在作業中の制作物: ${work.name}編 \n
+${content}
+`);
+			} else {
+				past.push(`
+ふぐおの過去の制作物: ${work.name}編 \n
+${content}
+`);
+			}
+		});
+	});
+	supplementJson.使用している技術.map((work) => {
+		work.description.map((content) => {
+			if (work.category.some((keyword) => word.includes(keyword))) {
+				now.push(`
+現在作業中の制作物で使用している技術: ${work.name}編 \n
+${content}
+`);
+			} else {
+				past.push(`
+ふぐおの過去の制作物で使用している技術: ${work.name}編 \n
+${content}
+`);
+			}
+		});
+	});
+	supplementJson.開発環境.map((work) => {
+		work.description.map((content) => {
+			past.push(`
+ふぐおの開発環境: ${work.name}編 \n
+${content}
+`);
+		});
+	});
+	supplementJson.プログラミング歴.map((content) => {
+		past.push(`
+ふぐおのプログラミング歴: \n
+${content}
+`);
+	});
+
+	return [...now, ...past];
 };
